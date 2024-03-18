de960a53e871662c55b69fdd56614a6963425860
Momen4444 <momen.qatr04@eng-st.cu.edu.eg>
2024-03-18 16:56:44 +0200

    MCQ Problems

---
+++ Problems.java
@@ -0,0 +1,61 @@
+public  class Problems
+{
+    public static void main(String[] args)
+    {
+        //every block has its own problem
+        {
+            //what is the output?
+            if (true)
+                break;
+        }
+
+
+
+
+        {
+            //what is the output
+            System.out.println('j' + 'a' + 'v' + 'a');
+        }
+
+
+
+
+        {
+            class Mobile
+            {
+                private int year;
+                String company_name;
+            }
+            public class new
+             {
+                public static void main(String[] args)
+                 {
+                     Mobile c=new Mobile();
+                     c.year=2018;
+                     c.company_name="samsung";
+                     System.out.println(c.company_name);
+                 }
+
+             }
+        }
+
+
+
+
+        {
+            class pen
+            {
+            final int price=44;
+            }
+            public class p
+            {
+                public static void main(String[] args)
+                {
+                    pen pp=new pen();
+                    pp.price=20;
+                    System.out.println(pp.price);
+                }
+            }
+        }
+    }
+}
