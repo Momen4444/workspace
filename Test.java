3ab030fd48f163cfe69b00c022bd0c7dd6039f97
Momen4444 <momen.qatr04@eng-st.cu.edu.eg>
2024-03-30 22:52:47 +0200

    Modified oop assigminet 1

--- Test.java
+++ Test.java
@@ -1,39 +1,60 @@
-import java.util.*;
+import java.util.Scanner;
 public class Test
 {
+
 public static void main(String[] args)
     {
-     System.out.println("Choose the number of parameters to pass to SUM:");
-     System.out.println("1\n2");
-     int sumRes = 0;
-     int op=getdata();
-     Scanner Obj = new Scanner(System.in);
+        Scanner Obj = new Scanner(System.in);
+        int sumRes = 0;
+        System.out.println("Choose the number of parameters to pass to SUM:");
+        System.out.println("1\n2");
+        int op=get_data(Obj);
         while(true)
         {
             if(op==1||op==2)
                 break;
             else
             {
-                System.out.println("inviled choice \ntry another one");
-                op=Obj.nextInt();
+                System.out.println("enter a invalid number");
+                op=get_data(Obj);
             }
 
         }
+        System.out.println("Result: " +calc(op,Obj,sumRes) );
+        Obj.close();
+     }
 
-        switch (op)
+    public static int get_data(Scanner o)
+    {
+        int z;
+        while(true)
+        {
+            try {
+                z = o.nextInt();
+                return z;
+            } catch (Exception e) {
+                System.out.println("you have entered wrong value,please enter a valid value");
+                o.nextLine();
+            }
+        }
+    }
+
+    public static int calc(int n,Scanner o,int sum)
+    {
+        switch (n)
         {
             case 1:
                 System.out.println("Choose the data type of parameter to pass to SUM:");
                 System.out.println("1. int\n2. double");
-                int select =getdata() ;
+                int select =get_data(o) ;
                 while(true)
                 {
                     if(select==1||select==2)
                         break;
                     else
                     {
-                        System.out.println("inviled choice \ntry another one");
-                        select=Obj.nextInt();
+                        System.out.println("invalid choice \ntry another one");
+                        select=get_data(o);
                     }
 
                 }
@@ -43,70 +64,51 @@ public static void main(String[] args)
                     {
                         System.out.println("Please enter an integer");
                         int input = 0;
-                        getdata();
-                        sumRes = sum(input);
+                        get_data(o);
+                        sum = sum(input);
                         break;
                     }
                     case 2:
                     {
                         System.out.println("Please enter a float");
-                        float input=0;
+                        float input;
                         while(true)
                         {
-                          try
-                          {
-                              Scanner O=new Scanner(System.in);
-                              input = O.nextFloat();
-                              O.close();
-                              break;
-                          }
-                          catch (Exception e)
-                          {
-                              System.out.println("Please enter a valid value");
-                          }
+                            try
+                            {
+                                input = o.nextFloat();
+                                break;
+                            }
+                            catch (Exception e)
+                            {
+                                System.out.println("Please enter a valid value");
+                                o.nextLine();
+                            }
                         }
-                        sumRes = sum(input);
+                        sum = sum(input);
                         break;
-                        }
                     }
-                    break;
-                case 2:
-                    System.out.println("Choose the two integers to pass to SUM:");
-                    int min =getdata();
-                    int max = getdata();
-                    if(min>max)
-                    {
-                     min=min^max;
-                     max=min^max;
-                     min=min^max;
-                    }
-                    sumRes = sum(min, max);
-                    break;
-                default:
-                    System.out.println("invalid input");
-            }
-        Obj.close();
-        System.out.println("Result: " + sumRes);
-     }
-
-    public static int getdata()
-    {
-        int z=0;
-        while(true)
-            try
-            {
-                Scanner myobj=new Scanner(System.in);
-                z=myobj.nextInt();
-                return z;
-            }
-        catch(Exception e)
-        {
-            System.out.println("you enterd wrong value,please enter a vaild value");
-
+                }
+                break;
+            case 2:
+                System.out.println("Choose the two integers to pass to SUM:");
+                int min =get_data(o);
+                int max = get_data(o);
+                if(min>max)
+                {
+                    min=min^max;
+                    max=min^max;
+                    min=min^max;
+                }
+                sum = sum(min, max);
+                break;
+            default:
+                System.out.println("invalid input");
         }
-
+        return sum;
     }
     // Method to calculate sum recursively for integer inputs
+
     public static int sum(int k) {
         if (k > 0) {
             return k + sum(k - 1);
@@ -116,6 +118,7 @@ public static void main(String[] args)
     }
 
     // Method to calculate sum for double inputs
+
     public static int sum(double k) {
         if (k > 0) {
             return (int) Math.round(k) + sum((int) k - 1);
@@ -124,6 +127,7 @@ public static void main(String[] args)
         }
     }
     // Method to calculate sum recursively for a range of integers
+
     public static int sum(int start, int end) {
         if (end > start) {
             return end + sum(start, end - 1);
